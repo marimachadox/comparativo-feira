3c6aa7bc21ab86aba056bd9457235a3d1aed81af
marianamachadox <marianalmachado09@gmail.com>
2021-08-07 22:28:22 -0300

    atualiza js

--- comparativo-feira.js
+++ comparativo-feira.js
@@ -4,44 +4,44 @@ function start(){
     var precoAtual= parseFloat(prompt("Por favor, insira o preço atual do produto! Use . (ponto) no lugar de , (vírgula)"));
     var precoAntigo = parseFloat(prompt("Por favor, insira o preço antigo do produto!Use . (ponto) no lugar de , (vírgula)"));
 
-    if (produto === 1 && precoAtual > precoAntigo){
+    if (produto === 1 && +precoAtual > +precoAntigo){
         alert("O tomate teve aumento de R$" +(precoAtual-precoAntigo));
 
     }
-    else if(produto === 1 && precoAtual < precoAntigo){
+    else if(produto === 1 && +precoAtual < +precoAntigo){
         alert("O tomate diminuiu R$"+(precoAntigo-precoAtual));
     }
-    else if (produto === 1 && precoAtual == precoAntigo){
+    else if (produto === 1 && +precoAtual == +precoAntigo){
         alert("O produto não teve alteração de preço")
         }
 
-    else if (produto === 2 && precoAtual > precoAntigo){
+    else if (produto === 2 && +precoAtual > +precoAntigo){
         alert("O abacaxi teve aumento de R$" + (precoAtual-precoAntigo));
     }
 
-    else if (produto === 2 && precoAtual < precoAntigo){
+    else if (produto === 2 && +precoAtual < +precoAntigo){
         alert("O abacaxi diminuiu R$ "+ (precoAntigo-precoAtual));
     }
-    else if (produto === 2 && precoAtual == precoAntigo){
+    else if (produto === 2 && +precoAtual == +precoAntigo){
     alert("O produto não teve alteração de preço")
     }
-    else if (produto === 3 && precoAtual > precoAntigo){
+    else if (produto === 3 && +precoAtual > +precoAntigo){
         alert("O limão teve aumento de R$"+ (precoAtual-precoAntigo));
     }
 
-    else if (produto === 3 && precoAtual < precoAntigo){
+    else if (produto === 3 && +precoAtual < +precoAntigo){
         alert("O limão diminuiu de R$ "+ (precoAntigo-precoAtual));
     }
-    else if (produto === 3 && precoAtual == precoAntigo){
+    else if (produto === 3 && +precoAtual == +precoAntigo){
         alert("O produto não teve alteração de preço")
         }
-    else if (produto === 4 && precoAtual > precoAntigo){
+    else if (produto === 4 && +precoAtual > +precoAntigo){
         alert("A laranja teve aumento de R$"+ (precoAntigo-precoAtual));
     }
-    else if (produto === 4 && precoAntigo < precoAtual){
-        alert("A laranja diminuiu de R$" + (precoAtual+precoAntigo));
+    else if (produto === 4 && +precoAntigo < +precoAtual){
+        alert("A laranja diminuiu de R$" + (precoAtual-precoAntigo));
     }
-    else if (produto === 4 && precoAtual == precoAntigo){
+    else if (produto === 4 && +precoAtual == +precoAntigo){
         alert("O produto não teve alteração de preço")
         }
     else{
